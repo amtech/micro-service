2eaaaf06791e36934937e233f5158c68eef5c64e
wangzebin <wangzebin@wangzebindeMacBook-Pro.local>
2020-04-10 11:00:03 +0800

    update

--- yaeger/main.go
+++ yaeger/main.go
@@ -20,7 +20,7 @@ func initJaeger() (tracer opentracing.Tracer, closer io.Closer, err error) {
 		// 设置采样参数
 		Sampler: &config.SamplerConfig{
 			Type:  "const", // 全采样模式
-			Param: 1,       // 开启全采样模式
+			Param: 1,       // 开启状态
 		},
 	}
 
@@ -34,53 +34,42 @@ func initJaeger() (tracer opentracing.Tracer, closer io.Closer, err error) {
 }
 
 // 检测用户余额，模拟子span任务
-func checkBalance(request string, ctx context.Context) {
+func CheckBalance(request string, ctx context.Context) {
 	// 创建子span
-	span, _ := opentracing.StartSpanFromContext(ctx, "checkBalance")
+	span, _ := opentracing.StartSpanFromContext(ctx, "CheckBalance")
 
 	// 模拟系统进行一系列的操作，耗时1/3秒
 	time.Sleep(time.Second / 3)
 
-	// 生成处理结果reply
-	reply := "checkBalance reply"
-
-	// 将需要追踪的信息放入tag
+	// 示例：将需要追踪的信息放入tag
 	span.SetTag("request", request)
-	span.SetTag("reply", reply)
+	span.SetTag("reply", "CheckBalance reply")
 
 	// 结束当前span
 	span.Finish()
 
-	// 打印输入数据结果
-	log.Println(request)
-	log.Println(reply)
+	log.Println("CheckBalance is done")
 }
 
 // 从用户账户扣款，模拟子span任务
-func reduction(request string, ctx context.Context) {
+func Reduction(request string, ctx context.Context) {
 	// 创建子span
+	span, _ := opentracing.StartSpanFromContext(ctx, "Reduction")
 
-	span, _ := opentracing.StartSpanFromContext(ctx, "reduction")
 	// 模拟系统进行一系列的操作，耗时1/2秒
 	time.Sleep(time.Second / 2)
 
-	// 生成处理结果reply
-	reply := "reduction reply"
-
-	// 将需要追踪的信息放入tag
+	// 示例：将需要追踪的信息放入tag
 	span.SetTag("request", request)
-	span.SetTag("reply", reply)
+	span.SetTag("reply", "Reduction reply")
 
 	// 结束当前span
 	span.Finish()
 
-	// 打印输入数据和输出结果
-	log.Println(request)
-	log.Println(reply)
+	log.Println("Reduction is done")
 }
 
 func main() {
-
 	// 初始化jaeger，创建一条新tracer
 	tracer, closer, err := initJaeger()
 	if err != nil {
@@ -88,23 +77,22 @@ func main() {
 	}
 	defer closer.Close()
 
-	// 创建一个新span，作为父span
-	span := tracer.StartSpan("CalculateAmount")
+	// 创建一个新span，作为父span，开始计费过程
+	span := tracer.StartSpan("CalculateFee")
 
-	// 设置一个标签信息tag
-	span.SetTag("db.instance", "customers")
-
-	// 设置一条日志信息log
-	span.LogKV("event", "timed out")
-
-	// 生成span的context
+	// 生成父span的context
 	ctx := opentracing.ContextWithSpan(context.Background(), span)
 
-	// 将父span的context作为参数，调用检测用户余额函数，其中生成子span
-	checkBalance("checkBalance request", ctx)
+	// 示例：设置一个span标签信息
+	span.SetTag("db.instance", "customers")
+	// 示例：输出一条span日志信息
+	span.LogKV("event", "timed out")
 
-	// 将父span的context作为参数，调用扣款函数，生成子span
-	reduction("reduction request", ctx)
+	// 将父span的context作为参数，调用检测用户余额函数
+	CheckBalance("CheckBalance request", ctx)
+
+	// 将父span的context作为参数，调用扣款函数
+	Reduction("Reduction request", ctx)
 
 	// 结束父span
 	span.Finish()
